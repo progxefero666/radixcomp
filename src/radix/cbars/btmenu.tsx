7b461553cce3dc3ea9fd6474410106a32d018451
progxefero666 <correoxefero@gmail.com>
2025-07-03 10:42:15 +0200

    a

--- src/radix/cbars/btmenu.tsx
+++ src/radix/cbars/btmenu.tsx
@@ -1,7 +1,7 @@
 //src\radix\cbars\btmenu.tsx
 
 import { Option } from "@/common/model/option";
-import { Flex, Button, } from "@radix-ui/themes";
+import { Flex, Button, Box, } from "@radix-ui/themes";
 import { InfoCircledIcon } from "@radix-ui/react-icons";
 import { ThemeButtonsStyle, ThemeCompStyle, ThemeMenusStyle } from "@/radix/radixtheme";
 import { radixTypeComp } from "../radixmodels";
@@ -14,46 +14,42 @@ export interface MenuButtonsProp {
     options: Option[]
     onselection: (name: string) => void;
 }
-export default function MenuButtons({options,actoption,onselection}: MenuButtonsProp) {
+export default function MenuButtons({ options, actoption, onselection }: MenuButtonsProp) {
 
-    const compStyle:radixTypeComp = ThemeCompStyle.CONT_STYLE;
+    const compStyle: radixTypeComp = ThemeCompStyle.CONT_STYLE;
 
-    const isActiveOption = (optionId:string,actoptionId:string|null) => {
-        if( (actoptionId!== null) && (actoptionId === optionId) ) {
+    const isActiveOption = (optionId: string, actoptionId: string | null) => {
+        if ((actoptionId !== null) && (actoptionId === optionId)) {
             return true;
-        } 
+        }
         return false;
     };
-	
 
-    
-    const renderButton = (id:string,
-                          onclick:(name:string)=>void,
-                          text:string,style:any ) => {
+    const renderButton = (id: string, onclick: (name: string) => void,
+        text: string, style: any) => {
         return (
             <Button key={id}
-                    onClick={() => onclick(id)}
-                    style={style}                                
-                    size={compStyle.size}
-                    variant={compStyle.variant} >
+                onClick={() => onclick(id)}
+                style={style}
+                size={compStyle.size}
+                variant={compStyle.variant} >
                 {text}
             </Button>
         )
     }
 
-    return (    
+    return (
         <Flex direction="column" gap="2" style={ThemeCompStyle.CONT_CSS_STYLE} >
             {options.map((section, index) => (
-                <div>
-                    {isActiveOption(section.id,actoption) ?
-                        renderButton(section.id,onselection,section.text,
-                                     ThemeMenusStyle.OPT_ACTIVE_COLOR_CSS_STYLE)
-                    :
-                        renderButton(section.id,onselection,section.text,
-                                     ThemeMenusStyle.OPT_COLOR_CSS_STYLE)                    
+                <Box key={index.toString()} >
+                    {isActiveOption(section.id, actoption) ?
+                        renderButton(section.id, onselection, section.text,
+                            ThemeMenusStyle.OPT_ACTIVE_COLOR_CSS_STYLE)
+                        :
+                        renderButton(section.id, onselection, section.text,
+                            ThemeMenusStyle.OPT_COLOR_CSS_STYLE)
                     }
-                    
-                </div>                
+                </Box>
             ))}
         </Flex>
     )
