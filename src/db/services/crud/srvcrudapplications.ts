99a3b8623ad534ff291d2ab3adfb49bb157b6fbc
progxefero666 <correoxefero@gmail.com>
2025-07-07 11:21:42 +0200

    a

--- src/db/services/crud/srvcrudapplications.ts
+++ src/db/services/crud/srvcrudapplications.ts
@@ -1,1 +1,100 @@
 //src\db\services\crud\srvcrudapplications.ts
+
+//src\db\services\crud\srvcrudapplications.ts
+"use server";
+
+import { JsonResponse } from "@/common/json/models/jsonresponse";
+import { PrismaClient } from "@generated/prisma";
+import { DbOps, OpUtil } from "@/db/dboperations";
+import { Application } from "@/db/dmmodels/application";
+import { parseItem } from "@/common/parsers/javascriptparser";
+import { DbTables } from "@/db/dbcatalog";
+
+
+/**
+ * __table__ insert
+ */
+export async function insert(item_serial:string): Promise<string> {
+    
+    const item: Application|null = parseItem<Application>(item_serial);
+    if(item===null){return JsonResponse.ERROR(DbOps.ERR_BADFORMAT);}
+
+    const prisma = new PrismaClient();
+    let result: object|null = null;
+    try {
+        result = await prisma.application.create({data:item as any});
+        if (result === null) {
+            return JsonResponse.ERROR
+                (OpUtil.getErrNotFoundMessage(DbOps.INSERT, DbTables.application));
+        }
+    }
+    catch (error) {
+        return JsonResponse.ERROR(OpUtil.getErrMessage(error));
+    }
+    finally {
+        await prisma.$disconnect();
+    }
+    return JsonResponse.SUCCESS(OpUtil.getOpName(DbTables.application,DbOps.INSERT), null);
+}//end
+
+/**
+ * __table__ update
+ */
+export async function update(item:Application): Promise<string> { 
+    const prisma = new PrismaClient();
+    let result = null;
+    try {
+        result = await prisma.application.update({where:{id:item.id!},data:item!});        
+        if (result === null) {
+            return JsonResponse.ERROR
+                (OpUtil.getErrNotFoundMessage(DbOps.UPDATE, DbTables.application));
+        }        
+    }
+    catch (error) {
+        return JsonResponse.ERROR(OpUtil.getErrMessage(error));
+    }
+    finally {await prisma.$disconnect();}
+
+    return JsonResponse.SUCCESS(OpUtil.getOpName("application", DbOps.UPDATE), null);
+}//end
+
+/**
+ * __table__ delete
+ */
+export async function delette(id: number): Promise<string> {
+    const prisma = new PrismaClient();
+    let result = null;
+    try {
+        result = await prisma.application.delete({where:{id:id}});
+        if (result === null) {
+            return JsonResponse.ERROR(OpUtil.getErrNotFoundMessage(DbOps.DELETE, DbTables.application));
+        }          
+    }
+    catch (error) {
+        return JsonResponse.ERROR(OpUtil.getErrMessage(error));
+    }
+    finally {await prisma.$disconnect();}
+
+    return JsonResponse.SUCCESS(OpUtil.getOpName("application", DbOps.DELETE),null);
+}//end
+
+/**
+ * __table__ delete all
+ */
+export async function deleteAll(): Promise<string> {
+    const prisma = new PrismaClient();
+    let result = null;
+    try {
+        result = await prisma.application.deleteMany({});
+        if (result === null) {
+            return JsonResponse.ERROR(OpUtil.getErrNotFoundMessage(DbOps.DELETE_ALL, DbTables.application));
+        }          
+    }
+    catch (error) {
+        return JsonResponse.ERROR(OpUtil.getErrMessage(error));
+    }
+    finally {await prisma.$disconnect();}
+
+    return JsonResponse.SUCCESS(OpUtil.getOpName("application", DbOps.DELETE_ALL),null);
+}//end
+
--- src/db/services/crud/srvcrudworkflows.ts
+++ src/db/services/crud/srvcrudworkflows.ts
@@ -1,1 +1,99 @@
 //src\db\services\crud\srvcrudworkflows.ts
+//src\db\services\crud\srvcrudworkflows.ts
+"use server";
+
+import { JsonResponse } from "@/common/json/models/jsonresponse";
+import { PrismaClient } from "@generated/prisma";
+import { DbOps, OpUtil } from "@/db/dboperations";
+import { Workflow } from "@/db/dmmodels/workflow";
+import { parseItem } from "@/common/parsers/javascriptparser";
+import { DbTables } from "@/db/dbcatalog";
+
+
+/**
+ * __table__ insert
+ */
+export async function insert(item_serial:string): Promise<string> {
+    
+    const item: Workflow|null = parseItem<Workflow>(item_serial);
+    if(item===null){return JsonResponse.ERROR(DbOps.ERR_BADFORMAT);}
+
+    const prisma = new PrismaClient();
+    let result: object|null = null;
+    try {
+        result = await prisma.workflow.create({data:item});
+        if (result === null) {
+            return JsonResponse.ERROR
+                (OpUtil.getErrNotFoundMessage(DbOps.INSERT, DbTables.workflow));
+        }
+    }
+    catch (error) {
+        return JsonResponse.ERROR(OpUtil.getErrMessage(error));
+    }
+    finally {
+        await prisma.$disconnect();
+    }
+    return JsonResponse.SUCCESS(OpUtil.getOpName(DbTables.workflow,DbOps.INSERT), null);
+}//end
+
+/**
+ * __table__ update
+ */
+export async function update(item:Workflow): Promise<string> { 
+    const prisma = new PrismaClient();
+    let result = null;
+    try {
+        result = await prisma.workflow.update({where:{id:item.id!},data:item!});        
+        if (result === null) {
+            return JsonResponse.ERROR
+                (OpUtil.getErrNotFoundMessage(DbOps.UPDATE, DbTables.workflow));
+        }        
+    }
+    catch (error) {
+        return JsonResponse.ERROR(OpUtil.getErrMessage(error));
+    }
+    finally {await prisma.$disconnect();}
+
+    return JsonResponse.SUCCESS(OpUtil.getOpName("workflow", DbOps.UPDATE), null);
+}//end
+
+/**
+ * __table__ delete
+ */
+export async function delette(id: number): Promise<string> {
+    const prisma = new PrismaClient();
+    let result = null;
+    try {
+        result = await prisma.workflow.delete({where:{id:id}});
+        if (result === null) {
+            return JsonResponse.ERROR(OpUtil.getErrNotFoundMessage(DbOps.DELETE, DbTables.workflow));
+        }          
+    }
+    catch (error) {
+        return JsonResponse.ERROR(OpUtil.getErrMessage(error));
+    }
+    finally {await prisma.$disconnect();}
+
+    return JsonResponse.SUCCESS(OpUtil.getOpName("workflow", DbOps.DELETE),null);
+}//end
+
+/**
+ * __table__ delete all
+ */
+export async function deleteAll(): Promise<string> {
+    const prisma = new PrismaClient();
+    let result = null;
+    try {
+        result = await prisma.workflow.deleteMany({});
+        if (result === null) {
+            return JsonResponse.ERROR(OpUtil.getErrNotFoundMessage(DbOps.DELETE_ALL, DbTables.workflow));
+        }          
+    }
+    catch (error) {
+        return JsonResponse.ERROR(OpUtil.getErrMessage(error));
+    }
+    finally {await prisma.$disconnect();}
+
+    return JsonResponse.SUCCESS(OpUtil.getOpName("workflow", DbOps.DELETE_ALL),null);
+}//end
+
