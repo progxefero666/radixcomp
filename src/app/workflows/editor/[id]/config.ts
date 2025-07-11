2e66cfa7a73c8ce2d555578d5176182979042cbe
progxefero666 <correoxefero@gmail.com>
2025-07-11 13:20:48 +0200

    d

---
+++ src/app/workflows/editor/[id]/config.ts
@@ -0,0 +1,36 @@
+//src\app\workflows\editor\config.ts
+
+
+
+/*	
+	Context
+	.................................
+	- Create Context
+	- Update Context	
+	- Upload File
+	
+	Groups Tasks Collection
+	.................................
+	- List groups
+	- Create group
+	- Update group
+	- Delete group
+	- Clear groups
+	
+	Task Collection
+	.................................	
+	- list tasks
+	- filter tasks
+	- Add Task
+	- Update Task
+	- Delete Task
+	- Move Task
+	- Duplicate Task
+	- Clear Tasks
+	- Upload File in Task
+*/
+export class WorkflowEditorConfig {
+
+
+
+}//end class
--- src/app/workflows/editor/[id]/pagecomp/wfeditor.tsx
+++ src/app/workflows/editor/[id]/pagecomp/wfeditor.tsx
@@ -1,32 +1,5 @@
 //src\app\workflows\editor\[id]\pagecomp\wfeditor.tsx
 
-/*	
-	Context
-	.................................
-	- Create Context
-	- Update Context	
-	- Upload File
-	
-	Groups Tasks Collection
-	.................................
-	- List groups
-	- Create group
-	- Update group
-	- Delete group
-	- Clear groups
-	
-	Task Collection
-	.................................	
-	- list tasks
-	- filter tasks
-	- Add Task
-	- Update Task
-	- Delete Task
-	- Move Task
-	- Duplicate Task
-	- Clear Tasks
-	- Upload File in Task
-*/
 
 //src\app\workflows\pagecomp\wfmain.tsx
 
