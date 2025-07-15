8c40b1a59dc3b1cac71ec204de141f7eead85dde
progxefero666 <correoxefero@gmail.com>
2025-07-15 08:46:10 +0200

    s

--- src/graph2d/shape/shapecylinder.ts
+++ src/graph2d/shape/shapecylinder.ts
@@ -1,8 +1,9 @@
 //src\graph2d\shape\shapecylinder.ts
 
 import { Dim2d, Point2d } from "@/graph2d/types2d";
-import { init } from "next/dist/compiled/webpack/webpack";
-import { ShapeUtil } from "../util/shapeutil";
+
+import { ShapeUtil } from "@/graph2d/util/shapeutil";
+import { XMath } from "@/common/math/xmath";
 
 
 /**
@@ -11,15 +12,16 @@ import { ShapeUtil } from "../util/shapeutil";
  */
 export class ShapeCylinder {
 
+    public static readonly INCLIN_PERCENT: number = 30; // 20% of the width
+
     public position: Point2d;
     public dim: Dim2d;
     public color: string;
 
     public rectPoints: Point2d[] = [];
+
     public ellipsesCenter: Point2d[] = [];
-
     public ellipsesRefPoint: Point2d[] = [];
-
     public ellipsesDim: Dim2d= {width: 0, height: 0};
 
     constructor(position:Point2d,dim:Dim2d,color:string) {
@@ -34,18 +36,26 @@ export class ShapeCylinder {
         this.ellipsesCenter[0] = {x: position.x, y: position.y - this.dim.height / 2};
         this.ellipsesCenter[1] = {x: position.x, y: position.y + this.dim.height / 2};
 
-        //y end = ± (4*radius)/3;
+        
         this.ellipsesRefPoint[0] = {
             x: this.ellipsesCenter[0].x, 
-            y: this.ellipsesCenter[0].y - (this.ellipsesDim.height*2)
+            y: this.ellipsesCenter[0].y + (this.ellipsesDim.height*2)
         };
         
         this.ellipsesRefPoint[1] = {
             x: this.ellipsesCenter[1].x, 
-            y: this.ellipsesCenter[1].y - (this.ellipsesDim.height*2)
+            y: this.ellipsesCenter[1].y + (this.ellipsesDim.height*2)
         };
 
     };//end
 
+    public init() {
+        this.rectPoints = ShapeUtil.getRectPoints(this.position,this.dim);     
+
+        //y end = ± (4*radius)/3;
+        this.ellipsesDim.width = this.dim.width;
+        const radiusBase:number = this.dim.width / 2;
+        const sizeCalcY:number = XMath.getValueOfPercent(radiusBase, ShapeCylinder.INCLIN_PERCENT);
+    };//end
 
 }//end class
