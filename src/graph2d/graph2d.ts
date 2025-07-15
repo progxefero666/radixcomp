cb5e726f61079a363fc8be1203ad250e0b639108
progxefero666 <correoxefero@gmail.com>
2025-07-15 09:44:24 +0200

    s

---
+++ src/graph2d/graph2d.ts
@@ -0,0 +1,19 @@
+//src\graph2d\graph2d.ts
+
+import { Dim2d, Point2d } from "@/graph2d/types2d";
+
+
+/**
+ * class Graph2d.DIM_DEF
+ */
+export class Graph2d {
+
+    public static readonly POINT_DEF:Point2d = {x:0,y:0};
+
+    public static readonly DIM_DEF:Dim2d     = {width:0,height:0};
+
+    public static init() {
+        // Initialization logic if needed
+    }
+
+}//end class Graph2d
--- src/graph2d/shape/shapecylinder.ts
+++ src/graph2d/shape/shapecylinder.ts
@@ -1,9 +1,8 @@
 //src\graph2d\shape\shapecylinder.ts
 
 import { Dim2d, Point2d } from "@/graph2d/types2d";
-
-import { ShapeUtil } from "@/graph2d/util/shapeutil";
-import { XMath } from "@/common/math/xmath";
+import { init } from "next/dist/compiled/webpack/webpack";
+import { ShapeUtil } from "../util/shapeutil";
 
 
 /**
@@ -12,50 +11,44 @@ import { XMath } from "@/common/math/xmath";
  */
 export class ShapeCylinder {
 
-    public static readonly INCLIN_PERCENT: number = 30; // 20% of the width
-
     public position: Point2d;
     public dim: Dim2d;
     public color: string;
+    public halfDimY: number = 0;
 
     public rectPoints: Point2d[] = [];
 
     public ellipsesCenter: Point2d[] = [];
-    public ellipsesRefPoint: Point2d[] = [];
-    public ellipsesDim: Dim2d= {width: 0, height: 0};
+    public ellipseARefPoint: Point2d[] = [];
+    public ellipseDownRefPoint: Point2d[] = [];
 
     constructor(position:Point2d,dim:Dim2d,color:string) {
         this.position = position;
         this.dim = dim;
         this.color = color;
-
+        
+        this.halfDimY   = Math.floor(this.dim.height / 2);
         this.rectPoints = ShapeUtil.getRectPoints(this.position, this.dim);
 
-        this.ellipsesDim.width = this.dim.width;
-        this.ellipsesDim.height = Math.floor(this.ellipsesDim.width / 5);
-        this.ellipsesCenter[0] = {x: position.x, y: position.y - this.dim.height / 2};
-        this.ellipsesCenter[1] = {x: position.x, y: position.y + this.dim.height / 2};
-
-        
-        this.ellipsesRefPoint[0] = {
-            x: this.ellipsesCenter[0].x, 
-            y: this.ellipsesCenter[0].y + (this.ellipsesDim.height*2)
-        };
-        
-        this.ellipsesRefPoint[1] = {
-            x: this.ellipsesCenter[1].x, 
-            y: this.ellipsesCenter[1].y + (this.ellipsesDim.height*2)
-        };
-
-    };//end
-
-    public init() {
-        this.rectPoints = ShapeUtil.getRectPoints(this.position,this.dim);     
+        this.ellipsesCenter[0] = {x: position.x, y: position.y - this.halfDimY};
+        this.ellipsesCenter[1] = {x: position.x, y: position.y + this.halfDimY};
 
         //y end = ± (4*radius)/3;
-        this.ellipsesDim.width = this.dim.width;
-        const radiusBase:number = this.dim.width / 2;
-        const sizeCalcY:number = XMath.getValueOfPercent(radiusBase, ShapeCylinder.INCLIN_PERCENT);
+
+        const sizeCalcY:number = this.dim.width / 5
+
+        this.ellipseARefPoint[0] = {
+            x: this.ellipsesCenter[0].x, 
+            y: this.ellipsesCenter[0].y + sizeCalcY
+        };
+        
+        this.ellipseDownRefPoint[0] = {
+            x: this.ellipsesCenter[1].x, 
+            y: this.ellipsesCenter[1].y + sizeCalcY
+        };
+
+    
     };//end
+
 
 }//end class
