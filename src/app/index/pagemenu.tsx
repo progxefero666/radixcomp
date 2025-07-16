eef996f381016d59961809f4586f1046aa8d4f17
progxefero666 <correoxefero@gmail.com>
2025-07-16 21:33:07 +0200

    x

--- src/app/index/pagemenu.tsx
+++ src/app/index/pagemenu.tsx
@@ -9,7 +9,7 @@ import { Box, Grid, Flex, Text } from "@radix-ui/themes";
 import PageGenCode from "../gencode/page";
 import PageTest from "../testcomp/page";
 import { Option } from "@/common/model/option";
-import Home from "./page";
+import Home from "../page";
 
 /**
  * Main app view page component
---
+++ src/app/workflows/page.tsx
@@ -0,0 +1,75 @@
+//src\app\workflows\page.tsx
+
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Box, Grid, Flex } from "@radix-ui/themes";
+import { AppWorkflowsConfig } from "@/front/appworkflows";
+import { Header } from "@/app/workflows/pagecomp/header";
+import { PrimaryBar } from "@/app/workflows/pagecomp/primarybar";
+import { WorkflowsManager } from "@/app/workflows/pagecomp/wfsmanager";
+import { SecondBar } from "@/app/workflows/pagecomp/secondbar";
+import { WorkflowViewer } from "./pagecomp/wfviewer";
+import { AppMemmory } from "@/front/appmemory";
+
+
+const layoutStyle = {
+    background: 'rgb(153, 17, 62)',
+    padding: '0',
+};
+
+/**
+ * Page Workflows Manegement
+ *  const router = useRouter();
+ */
+export default function PageWorkflows() {
+
+    const router = useRouter();
+    const [actsection, setActSection] = useState<string>(AppWorkflowsConfig.MOD_SECTIONS.MANAGER_WORKFLOWS.id);   
+    const [workflowId, setWorkflowId] = useState<number | null>(null);
+        
+    const onSectionSelected = (section: string) => {
+        setActSection(section);
+    };
+
+    const openWorkflow = (id:number) => {
+        AppMemmory.saveWorkflowId(id);
+        router.push("/workflows/wfeditor");
+    };
+
+    const viewWorkflow = (id:number) => {
+        setWorkflowId(id);
+    };
+
+    return (
+        <Grid height="100vh" rows="auto 1fr" columns="14% 41% 41% 4%" style={layoutStyle} >
+
+            <Flex gridColumn="1/5" gridRow="1" >
+                <Header section={actsection} />
+            </Flex>
+
+            <Flex gridColumn="1" gridRow="2" >
+                <PrimaryBar section={actsection} onselection={onSectionSelected} />
+            </Flex>
+
+            <Flex gridColumn="2" gridRow="2" >
+                <WorkflowsManager section={actsection}
+                                  editworkflow={openWorkflow}
+                                  viewworkflow={viewWorkflow} />
+            </Flex>
+
+            <Flex gridColumn="3" gridRow="2" >
+                {(workflowId!==null) ?
+                    <WorkflowViewer workflowId={workflowId}/>
+                    : <Box width="100%">not data charged</Box>}
+            </Flex>
+
+            <Flex gridColumn="4" gridRow="2" >
+                <SecondBar section={actsection} />
+            </Flex>
+
+        </Grid>
+    );
+
+}//end page
